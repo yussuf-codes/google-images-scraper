c309f5f9ecac1f8f852b09b87037b6518c751dee
yussuf-codes <yussuf.code@gmail.com>
2024-05-08 04:16:58 +0300

    Stable version

--- src/google_images_scraper/implementation.py
+++ src/google_images_scraper/implementation.py
@@ -3,18 +3,21 @@ from os import mkdir, path
 from urllib.request import urlretrieve
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 
 def ScrapeImages(search_query: str, images_number: int, download_location: str) -> None:
-    THUMBNAIL_CLASS_NAME: str = "Q4LuWd"
+    THUMBNAIL_CLASS_NAME: str = "GMCzAd"
     BUTTON_CLASS_NAME: str = "FAGjZe"
     IMAGE_CLASS_NAME: str = "iPVvYb"
-    RESULTS_END_CLASS_NAME: str = "OuJzKb"
-    
-    images_number *= 1.4
-    images_number = int(images_number)
 
-    driver: Chrome = Chrome()
+    number: float = float(images_number) * 1.2
+    number = int(number)
+
+    options = Options()
+    options.add_argument("lang=en-US")
+
+    driver: Chrome = Chrome(options)
 
     driver.get("https://images.google.com/")
 
@@ -25,20 +28,24 @@ def ScrapeImages(search_query: str, images_number: int, download_location: str) 
     search_bar.send_keys(search_query)
     search_bar.submit()
 
-    while len(driver.find_elements(By.CLASS_NAME, THUMBNAIL_CLASS_NAME)) <= images_number and not driver.find_element(By.CLASS_NAME, RESULTS_END_CLASS_NAME).is_displayed():
+    while len(driver.find_elements(By.CLASS_NAME, THUMBNAIL_CLASS_NAME)) <= number:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         try:
             driver.find_element(By.CLASS_NAME, BUTTON_CLASS_NAME).click()
         except Exception:
             pass
 
-    thumbnails = driver.find_elements(By.CLASS_NAME, THUMBNAIL_CLASS_NAME)[:images_number]
+    thumbnails = driver.find_elements(By.CLASS_NAME, THUMBNAIL_CLASS_NAME)[:number]
 
     urls: list[str] = []
+
     for thumbnail in thumbnails:
         try:
             thumbnail.click()
-            urls.append(driver.find_element(By.CLASS_NAME, IMAGE_CLASS_NAME).get_attribute("src"))
+            url: str | None = driver.find_element(By.CLASS_NAME, IMAGE_CLASS_NAME).get_attribute("src")
+            if url is None:
+                continue
+            urls.append(url)
         except Exception:
             pass
 
